195d1e4843e5c0331ae0937671ce2f8f200be558
chatelao <2461532+chatelao@users.noreply.github.com>
2026-09-16 11:57:52 +0000

    Verify xschem IHP SG13G2 compliance and update lvs.tcl

--- tcl/lvs.tcl
+++ tcl/lvs.tcl
@@ -1,8 +1,8 @@
 set layout [readnet spice $project.lvs.spice]
 set schem  [readnet verilog /dev/null]
-readnet spice $::env(PDK_ROOT)/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $schem
+readnet spice $::env(PDK_ROOT)/ihp-sg13g2/libs.ref/sg13g2_stdcell/spice/sg13g2_stdcell.spice $schem
 readnet spice   ../xschem/simulation/mosbius.spice $schem
 readnet verilog ../src/ctrl_top.synth.v            $schem
 readnet verilog ../src/project.v                   $schem
 ::netgen::format 60
-lvs "$layout $project" "$schem $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
+lvs "$layout $project" "$schem $project" $::env(PDK_ROOT)/ihp-sg13g2/libs.tech/netgen/sg13g2_setup.tcl lvs.report -blackbox
